69c4454b3cc7594216e94fbe04692530cdc4f69d
vap0rtranz <vap0rtranz@gmail.com>
2016-02-11 17:21:19 -0600

    adding RDS, change EC2 format

--- inventory/ec2-instance-list.py
+++ inventory/ec2-instance-list.py
@@ -5,39 +5,33 @@ import boto.ec2
 access_key = ''
 secret_key = ''
 
-def get_ec2_instances(region):
-    ec2_conn = boto.ec2.connect_to_region(region,
-                aws_access_key_id=access_key,
-                aws_secret_access_key=secret_key)
+def get_ec2_instances(region,prod_env_tags,nonprod_env_tags):
+    ec2_conn = boto.ec2.connect_to_region(region, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
     reservations = ec2_conn.get_all_reservations()
-    #for reservation in reservations:    
-    #    print region+':',reservation.instances,
-    instances = [i for r in reservations for i in r.instances]
-    for i in instances:
+    instances = [instance for r in reservations for instance in r.instances]
+    for instance in instances:
       #pprint(i.__dict__)
-      if i.platform is None:
+      if instance.platform is None:
         platform = 'linux'
       else:
         platform = 'windows'
-      p_env_label = [ 'PROD', 'UAT', 'STAG', 'Stag', 'prod', 'uat' ]
-      np_env_label = [ 'QA', 'DEV', 'dev' ]
-      if any(env in p_env_label for env in i.tags):
+      if any(env in prod_env_tags for env in instance.tags):
         environment = 'PROD/UAT'
-      elif any(env in np_env_label for env in i.tags):
+      elif any(env in nonprod_env_tags for env in instance.tags):
         environment = 'DEV'
       else:
         environment = 'Unknown'
-      #mytags = str(i.tags)
-      #list_of_words = mytags.split()
-      #iname = list_of_words[list_of_words.index("Name") + 1]
-      print(i.id, i.instance_type, i.region, environment, platform, i.tags)
-
-    #for vol in ec2_conn.get_all_volumes():
-    #    print region+':',vol.id
+      instance.id = str(instance.id)
+      instance.instance_type = str(instance.instance_type)
+      instance.tags = str(instance.tags)
+      print instance.id, instance.instance_type, instance.placement, environment, platform, instance.tags
+      #print(i.id, i.instance_type, i.region)
 
 def main():
     regions = [ 'us-east-1','us-west-1','us-west-2','eu-west-1','sa-east-1',
                 'ap-southeast-1','ap-southeast-2','ap-northeast-1', 'eu-central-1', 'ap-northeast-2' ]
+    prod_env_tags = [ 'PROD', 'UAT', 'STAG', 'Stag', 'prod', 'uat' ]
+    nonprod_env_tags = [ 'QA', 'DEV', 'dev' ]
     parser = argparse.ArgumentParser()
     parser.add_argument('access_key', help='Access Key');
     parser.add_argument('secret_key', help='Secret Key');
@@ -47,6 +41,7 @@ def main():
     access_key = args.access_key
     secret_key = args.secret_key
     
-    for region in regions: get_ec2_instances(region)
+    print("EC2 ID, Instance type, AZ, Env, Platform, Tags")
+    for region in regions: get_ec2_instances(region,prod_env_tags,nonprod_env_tags)
 
 if  __name__ =='__main__':main()
---
+++ inventory/rds-instance-list.py
@@ -0,0 +1,42 @@
+import argparse
+from pprint import pprint
+import boto.rds
+import unicodedata
+
+access_key = ''
+secret_key = ''
+
+def get_rds_instances(region,fields):
+    rds_conn = boto.rds.connect_to_region(region, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+    dbinstances = rds_conn.get_all_dbinstances()
+    for instance in dbinstances:
+      #pprint(i.__dict__)
+# trying to use variable of class DBInstance as string to sanitize in loop fails 
+      #for field in fields: 
+        #instance."field" = str(instance."field")
+      #  print(instance.field)
+      instance.id = str(instance.id)
+      instance.instance_class = str(instance.instance_class) 
+      instance.engine = str(instance.engine)
+      instance.multi_az = str(instance.multi_az)
+      instance.availability_zone = str(instance.availability_zone)
+      instance.allocated_storage = str(instance.allocated_storage)
+      print instance.id, instance.instance_class, instance.engine, instance.multi_az, instance.availability_zone, instance.allocated_storage
+
+def main():
+    regions = [ 'us-east-1','us-west-1','us-west-2','eu-west-1','sa-east-1',
+                'ap-southeast-1','ap-southeast-2','ap-northeast-1', 'eu-central-1', 'ap-northeast-2' ]
+    fields = [ 'id', 'instance_class', 'engine', 'multi_az', 'availability_zone', 'allocated_storage' ]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('access_key', help='Access Key');
+    parser.add_argument('secret_key', help='Secret Key');
+    args = parser.parse_args()
+    global access_key
+    global secret_key
+    access_key = args.access_key
+    secret_key = args.secret_key
+    
+    print("DB ID, Instance class, Engine, MultiAZ, AZ, Storage")
+    for region in regions: get_rds_instances(region,fields)
+
+if  __name__ =='__main__':main()
